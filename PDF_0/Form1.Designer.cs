42cb6aa1af602c4bd8120a45bb9cf08a09257a28
Keinz <keima_k@icloud.com>
2024-02-03 05:34:07 +0900

    プロジェクト ファイルを追加します。

---
+++ PDF_0/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PDF_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(149, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ファイル結合を行う";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ PDF_0/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PDF_0
+{
+    public partial class Form1 : Form
+    {
+        //PDFの結合えお行うプログラム
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string DesktopFilepath = "C:\\Users\\" + Environment.UserName + "\\Desktop\\";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Open_setting();
+        }
+        void Open_setting()
+        {
+            openFileDialog1.DefaultExt = "pdf";
+            openFileDialog1.Filter = "PDF|*.pdf*";
+            openFileDialog1.Title = "結合するファイルを選択する";
+            saveFileDialog1.DefaultExt = "pdf";
+            saveFileDialog1.Filter = "PDF|*.pdf*";
+            saveFileDialog1.Title = "結合したファイルを保存する";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            string[] files = openFileDialog1.FileNames;
+            //結合後の出力ファイル
+            PdfDocument outputPdfDocument = new PdfDocument();
+            foreach (string file in files)
+            {
+                //ファイルを開く
+                PdfDocument inputPdfDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+                //１ページずつ出力ファイルに追加
+                for (int i = 0; i < inputPdfDocument.PageCount; i++)
+                {
+                    outputPdfDocument.AddPage(inputPdfDocument.Pages[i]);
+                }
+                //ファイルを閉じる
+                inputPdfDocument.Close();
+            }
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            //出力
+            outputPdfDocument.Save(saveFileDialog1.FileName);
+        }
+    }
+}
