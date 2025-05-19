c9b1afdcbf39978422029a92848c6e6c8a11a8b8
Keinz-K <keima_k@icloud.com>
2025-05-19 11:29:48 +0900

    機能追加に向けたコードの記述 (まだ実装できてない)

--- PDF_0/Form1.Designer.cs
+++ PDF_0/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -43,22 +45,33 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(30, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(50, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 19);
+            this.label1.Size = new System.Drawing.Size(212, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "ファイル結合を行う";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(337, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ファイル分割を行う";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(212, 51);
+            this.ClientSize = new System.Drawing.Size(668, 87);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -72,6 +85,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- PDF_0/Form1.cs
+++ PDF_0/Form1.cs
@@ -7,7 +7,7 @@ namespace PDF_0
 {
     public partial class Form1 : Form
     {
-        //PDFの結合を行うプログラム
+        //PDFの結合・分割を行うプログラム
         //このプロジェクトのように、追加パッケージを入れたものは
         //実行ファイルのみで動くわけがないことに注意しなければならない(1敗)
         public Form1()
@@ -32,6 +32,7 @@ namespace PDF_0
         }
         private void label1_Click(object sender, EventArgs e)
         {
+            openFileDialog1.Multiselect = true;
             if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
             string[] files = openFileDialog1.FileNames;
             //結合後の出力ファイル
@@ -41,16 +42,48 @@ namespace PDF_0
                 //ファイルを開く
                 PdfDocument inputPdfDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
                 //１ページずつ出力ファイルに追加
-                for (int i = 0; i < inputPdfDocument.PageCount; i++)
+                for (int i = 0; i < inputPdfDocument.PageCount-1; i++)
                 {
                     outputPdfDocument.AddPage(inputPdfDocument.Pages[i]);
                 }
                 //ファイルを閉じる
-                inputPdfDocument.Close();
+                //inputPdfDocument.Close();
             }
             if (saveFileDialog1.ShowDialog() == DialogResult.Cancel) return;
             //出力
             outputPdfDocument.Save(saveFileDialog1.FileName);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "分割するファイルを指定";
+            openFileDialog1.Multiselect = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            // 元のPDFファイルのパス
+            string inputFilePath = openFileDialog1.FileName;
+
+            // PDFを読み込み（修正可能として開く）
+            PdfDocument inputDocument = PdfReader.Open(inputFilePath, PdfDocumentOpenMode.Import);
+
+            // 各ページごとに新しいPDFを作成して保存
+            for (int idx = 0; idx < inputDocument.PageCount; idx++)
+            {
+                // 新しいPDFドキュメント作成
+                PdfDocument outputDocument = new PdfDocument();
+                outputDocument.Version = inputDocument.Version;
+                outputDocument.Info.Title = $"Page {idx + 1} of {inputDocument.Info.Title}";
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+
+                // 対象ページを追加
+                outputDocument.AddPage(inputDocument.Pages[idx]);
+
+                // ファイル名を作成
+                string outputFilePath = $"output_page_{idx + 1}.pdf";
+
+                // 保存
+                outputDocument.Save(outputFilePath);
+                Console.WriteLine($"Saved: {outputFilePath}");
+            }
         }
     }
 }
